8e273ee00c7e0fd81f69d2831c3f100c773c2030
Tushar Singh <bt24csd001gmail@example.com>
2025-07-26 20:34:13 +0530

    addingNxtAuth

--- mstrymsg/src/app/api/auth/[...nextauth]/opctions.ts
+++ mstrymsg/src/app/api/auth/[...nextauth]/opctions.ts
@@ -1,11 +1,11 @@
 import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"; // ✅ fixed import (default, not named)
+import CredentialsProvider from "next-auth/providers/credentials"; // ✅ default import
 
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 
-export const authOpctions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       id: "credentials",
@@ -20,12 +20,12 @@ export const authOpctions: NextAuthOptions = {
           const user = await UserModel.findOne({
             $or: [
               { email: credentials.email },
-              { username: credentials.email },
+              { username: credentials.email }, // search by email or username
             ],
           });
 
           if (!user) {
-            throw new Error("No user found with this email");
+            throw new Error("No user found with this email or username");
           }
 
           if (!user.isVerified) {
@@ -40,12 +40,40 @@ export const authOpctions: NextAuthOptions = {
           if (isPasswordCorrect) {
             return user;
           } else {
-            throw new Error("Incorrect Password");
+            throw new Error("Incorrect password");
           }
         } catch (error: any) {
-          throw new Error(error.message || "Something went wrong");
+          throw new Error(error.message || "Login failed");
         }
       },
     }),
   ],
+  callbacks:{
+ 
+    async jwt({ token, user }) {
+      if (user) {
+        token.id=user._id?.toString()
+        token.isVerified=user.isverified;
+        token.isAcceptingMessages=user.isAcceptingMessages;
+        token.username=user.username;
+
+      }
+      return token
+    } ,
+  async session({ session,  token }) {
+    if (token) {
+      session.user._id=token._id
+      session.user.isVerified=token.isVerified
+      session.user.isAcceptingMessages=token.isAcceptingMessages
+      session.user.username=token.username
+    }
+  return session
+    }, },
+  pages:{
+    signIn:'/sign-in'
+  },
+  session:{
+strategy:"jwt"
+  },
+  secret:process.env.NEXTAUTH_SECRET
 };
